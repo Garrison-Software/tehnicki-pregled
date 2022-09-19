03d90322c36f03b8f5f50b18e0ce15e22170df63
DimitrijeZivojinovic <dimitrijezivojinovic@hotmail.com>
2022-09-19 13:33:16 +0200

    all new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./sections/Header/Header";
+import NavMenu from "./sections/NavigationMenu/NavMenu";
+import Hero from "./sections/HeroBanner/Hero";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <NavMenu />
+      <Hero/>
     </div>
   );
 }
