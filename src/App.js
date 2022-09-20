6222ccf57de74eb9da1b2ead0002eb0da0d363f7
BokiThe <ristic1989@gmail.com>
2022-09-20 15:34:13 +0200

    karticaa napravljena

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+
 import "./App.css";
 import Header from "./sections/Header/Header";
 import NavMenu from "./sections/NavigationMenu/NavMenu";
 import Hero from "./sections/HeroBanner/Hero";
 import AboutUs from "./sections/AboutUs/AboutUs";
+import Card from "./components/card/Card";
+import img1 from "./assets/icons/repair/pexels-cottonbro-4489765.jpg"
+
 
 function App() {
   return (
@@ -11,6 +15,23 @@ function App() {
       <NavMenu />
       <Hero/>
       <AboutUs/>
+      <div>
+    
+      <Card
+      src={img1}
+      alt="alt"
+      price="100"
+      title="tehnicki pregled"
+      href1="#"
+      href2="#"
+      Link1Text="Get a Quote"
+      Link2Text="reaad more"
+      overlayTitle="Tehnicki pregled"
+      overlaySubtitle=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat blanditiis ut in quasi nesciunt deleniti amet, libero repellat, eius facilis quas reprehenderit eveniet ipsum enim? Rerum quam consequatur qui perferendis."
+      overlayHref="#"
+      overlayLinkText="read More"
+      />
+      </div>
     </div>
   );
 }
