a7aed55edd9d1a5930db99a67802968333e3ad3b
DimitrijeZivojinovic <dimitrijezivojinovic@hotmail.com>
2022-09-20 03:23:35 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import Header from "./sections/Header/Header";
 import NavMenu from "./sections/NavigationMenu/NavMenu";
 import Hero from "./sections/HeroBanner/Hero";
+import Footer from "./sections/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <NavMenu />
-      <Hero/>
+      <Hero />
+      <Footer />
     </div>
   );
 }
