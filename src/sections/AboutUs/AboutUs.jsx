dded63d63d959b0b0eb572dbb76b35e2b5d2bab9
9Boxa4 <bogdanjovanovic10@gmail.com>
2022-09-20 14:17:48 +0200

    resolving conflicts

--- src/sections/AboutUs/AboutUs.jsx
+++ src/sections/AboutUs/AboutUs.jsx
@@ -9,10 +9,28 @@ const AboutUs = () => {
           <h2>Zašto izabrati baš nas?</h2>
           <h3>Pogledajte naše ponude i pogodnosti</h3>
         </div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repudiandae eligendi quaerat nihil possimus repellat nam dolorem, hic voluptatum perspiciatis dignissimos tenetur beatae eius rerum ducimus. A quidem vel culpa!
-        Dolorem impedit corporis similique veritatis! Officiis voluptas laborum magni deserunt dolores? Totam quaerat corporis quidem consequuntur adipisci eius repudiandae aperiam, veritatis eos harum nemo voluptates est atque dolorum, rem nobis.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+          repudiandae eligendi quaerat nihil possimus repellat nam dolorem, hic
+          voluptatum perspiciatis dignissimos tenetur beatae eius rerum ducimus.
+          A quidem vel culpa! Dolorem impedit corporis similique veritatis!
+          Officiis voluptas laborum magni deserunt dolores? Totam quaerat
+          corporis quidem consequuntur adipisci eius repudiandae aperiam,
+          veritatis eos harum nemo voluptates est atque dolorum, rem nobis.
+        </p>
       </div>
-      <div className={styles.aboutPromotions}></div>
+      <div className={styles.aboutPromotions}>
+        <div className={styles.blockSet1}>
+            <div className={styles.promoBlock}></div>
+            <div className={styles.promoBlock}></div>
+        </div>
+
+        <div className={styles.blockSet2}>
+            <div className={styles.promoBlock}></div>
+            <div className={styles.promoBlock}></div>
+        </div>
+
+      </div>
     </div>
   );
 };
