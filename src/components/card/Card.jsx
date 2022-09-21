63ff609e11021dbd7daac6c60f32860c59d2daaf
BokiThe <ristic1989@gmail.com>
2022-09-21 13:40:35 +0200

    caard functionality aadded

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,26 +1,32 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useRef} from 'react';
 import card from './card.module.css';
 
 const Card = (props) => {
     const [isFlipped, setIsFlipped] = useState(false);
-    const innerCard = document.querySelector(".card_overlayWraper__XVNwS")
+    const innerCard = useRef("")
     const handleMouseEnter = (e)=>{
+    e.preventDefault();
+    if(!isFlipped){
         setIsFlipped(true)
-        innerCard.setAttribute("style", "display:flex;")
+        innerCard.current.setAttribute("style", "display: flex") 
+    }
+   
         
     }
     const handleMouseLeave = (e) => {
+        e.preventDefault()
+        innerCard.current.setAttribute("style", "display: none")
         setIsFlipped(false)
-        innerCard.setAttribute("style", "display:none; ")
+        
     }
 
   return (
     <div className={card.cardWraper}  >
-        <div className={card.cardImgWraper}onMouseEnter={handleMouseEnter}><img src={props.src} alt={props.alt} /> <span className={card.cardPrice}>{props.price}din</span></div>
+        <div className={card.cardImgWraper} onMouseEnter={handleMouseEnter} ><img src={props.src} alt={props.alt} name="img" /> <span className={card.cardPrice}>{props.price}din</span></div>
         <div className={card.cardTitle}><span className={card.cardTitleLogo}></span><span className={card.cardTitleText}>{props.title}</span> </div>
         <div className={card.cardSubtitle}><a href={props.href1} className={card.cardQuote}>{props.Link1Text}</a> <a href={props.href2} className={card.cardReadMore}>{props.Link2Text}</a></div>
 
-        <div className={card.overlayWraper} onMouseLeave={handleMouseLeave}>
+        <div className={card.overlayWraper}ref={innerCard} onMouseLeave={handleMouseLeave}>
             <h1 className={card.ovelayTitle}>{props.overlayTitle}</h1>
             <p className={card.overlaySubtitle}>{props.overlaySubtitle}</p>
             <a className={card.overlayLink} href={props.overlayHref}>{props.overlayLinkText}</a>
