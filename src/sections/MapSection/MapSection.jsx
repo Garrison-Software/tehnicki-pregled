44141acf1517f0431a18776ae455736e9e8534cf
9Boxa4 <bogdanjovanovic10@gmail.com>
2022-09-30 14:26:03 +0200

    added google map on wrong address  will be updated

---
+++ src/sections/MapSection/MapSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+const MapSection = () => {
+  return (
+    <div class="mapouter">
+        <div style={{backgroundColor:"#292929", paddingTop:"50px"}}
+        class="gmap_canvas">
+       <iframe width="800" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=sokolska%204%20nis&t=&z=17&ie=UTF8&iwloc=&output=embed"  scrolling="no" >
+        </iframe>
+        <a href="https://putlocker-is.org"></a>
+    </div>
+    </div>
+  )
+}
+
+export default MapSection
+
+
